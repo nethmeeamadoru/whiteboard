003362869ea958c2ce098a86697973a0fb791e3b
Alex Savia <alex.savia@aalto.fi>
2023-03-15 16:15:52 +0200

    Add check for users roomstatus to stop backend crashing when user not joined and does something.

--- server/index.js
+++ server/index.js
@@ -70,83 +70,87 @@ function broadcastMessageToUser(json, userId) {
   }
 }
 
-function broadcastMessageToUserAlreadyJsonFormatted(json, userId) {
-  // We are sending the current data to specific client in
-  const data = JSON.stringify(json)
-  let client = userToClients[userId]
-  if (client.readyState === WebSocket.OPEN) {
-    client.send(data)
-  }
-}
-
 function handleMessage(message, userId) {
   const dataFromClient = JSON.parse(message.toString())
   const json = { type: dataFromClient.type }
-  if (dataFromClient.type === typesDef.CREATE_NEW_ROOM) {
-    const roomId = uuidv4()
-    console.log(`Creating new room ${roomId}`)
-    roomToUsers[roomId] = [userId]
-    userToRoom[userId] = roomId
-    userIdToUsername[userId] = dataFromClient.username
-    roomToEvents[roomId] = []
-    //roomUserActivity[roomId] = [`${dataFromClient.username} created the whiteboard`]
-    json.data = { roomId: roomId }
-    broadcastMessageToRoom(json, roomId)
-  }
-  else if (dataFromClient.type === typesDef.ASK_TO_JOIN_ROOM) {
-    const roomId = dataFromClient.roomId
-    userIdToUsername[userId] = dataFromClient.username
-    if (roomId && roomId in roomToUsers) {
-      console.log(`User ${userId} asking to join room ${roomId}`)
-      const roomOwnerId = roomToUsers[roomId][0]
-      json.data = { username: userIdToUsername[userId], userId: userId }
-      broadcastMessageToUser(json, roomOwnerId)
+
+  let roomId = userToRoom[userId]
+
+  // If user does not belong to a room only certain operation are supposed to be possible:
+  // For example create room and ask to join.
+  if (!roomId) {
+    if (dataFromClient.type === typesDef.CREATE_NEW_ROOM) {
+      roomId = uuidv4()
+      console.log(`Creating new room ${roomId}`)
+      roomToUsers[roomId] = [userId]
+      userToRoom[userId] = roomId
+      userIdToUsername[userId] = dataFromClient.username
+      roomToEvents[roomId] = []
+      json.data = { roomId: roomId }
+      broadcastMessageToRoom(json, roomId)
+    }
+    else if (dataFromClient.type === typesDef.ASK_TO_JOIN_ROOM) {
+      roomId = dataFromClient.roomId
+      userIdToUsername[userId] = dataFromClient.username
+      if (roomId && roomId in roomToUsers) {
+        console.log(`User ${userId} asking to join room ${roomId}`)
+        const roomOwnerId = roomToUsers[roomId][0]
+        json.data = { username: userIdToUsername[userId], userId: userId }
+        broadcastMessageToUser(json, roomOwnerId)
+      }
+      else {
+        console.log(`User ${userId} asked to join nonexisting room ${roomId}`)
+        json.type = typesDef.REJECT_JOIN_TO_ROOM
+        json.data = { }
+        broadcastMessageToUser(json, userId)
+      }
     }
     else {
-      console.log(`User ${userId} asked to join nonexisting room ${roomId}`)
-      json.type = typesDef.REJECT_JOIN_TO_ROOM
-      json.data = { }
-      broadcastMessageToUser(json, userId)
+      console.log('Unknown event when user does not belong to any room')
+      console.log(userId)
+      console.log(dataFromClient)
     }
   }
-  else if (dataFromClient.type === typesDef.REJECT_JOIN_TO_ROOM) {
-    const rejectedUserId = dataFromClient.userId
-    console.log(`Roomowner ${userId} rejected user ${rejectedUserId}`)
-    json.data = { }
-    broadcastMessageToUser(json, rejectedUserId)
-  }
-  else if (dataFromClient.type === typesDef.ADD_USER_TO_ROOM) {
-    const roomId = userToRoom[userId]
-    const userIdToJoin = dataFromClient.userId
-    // Only room owner can add other users
-    if (userId === roomToUsers[roomId][0]) {
-      console.log(`Adding user ${userIdToJoin} to room ${roomId}`)
-      roomToUsers[roomId].push(userIdToJoin)
-      userToRoom[userIdToJoin] = roomId
-      json.data = { username: userIdToUsername[userIdToJoin] }
-      broadcastMessageToRoom(json, roomId)
-
-      // Send all preexisting draw data to just joined user.
-      for (const jsonEvent of roomToEvents[roomId]) {
-        broadcastMessageToUserAlreadyJsonFormatted(jsonEvent, userIdToJoin)
-      }
-    } else {
-      console.log('Error: Only room owner can add other users.')
-    }
-  }
-  else if (dataFromClient.type === typesDef.WHITEBOARD_DRAW) {
-    console.log('Whiteboard event.')
-    const roomId = userToRoom[userId]
-
-    // Store events as they arrive to roomspecific array
-    roomToEvents[roomId].push(dataFromClient)
-
-    broadcastMessageToRoom(dataFromClient, roomId)
-  }
+  // Following action require that user belongs to a room:
   else {
-    console.log('Unknown event')
-    console.log(userId)
-    console.log(dataFromClient)
+    if (dataFromClient.type === typesDef.REJECT_JOIN_TO_ROOM) {
+      const rejectedUserId = dataFromClient.userId
+      console.log(`Roomowner ${userId} rejected user ${rejectedUserId}`)
+      json.data = { }
+      broadcastMessageToUser(json, rejectedUserId)
+    }
+    else if (dataFromClient.type === typesDef.ADD_USER_TO_ROOM) {
+      const userIdToJoin = dataFromClient.userId
+      // Only room owner can add other users
+      if (userId === roomToUsers[roomId][0]) {
+        console.log(`Adding user ${userIdToJoin} to room ${roomId}`)
+        roomToUsers[roomId].push(userIdToJoin)
+        userToRoom[userIdToJoin] = roomId
+        json.data = { username: userIdToUsername[userIdToJoin] }
+        broadcastMessageToRoom(json, roomId)
+
+        // Send all preexisting draw data to just joined user.
+        // TODO: send clear event first to the new user to make sure that whiteboard is empty.
+        for (const jsonEvent of roomToEvents[roomId]) {
+          broadcastMessageToUser(jsonEvent, userIdToJoin)
+        }
+      } else {
+        console.log('Error: Only room owner can add other users.')
+      }
+    }
+    else if (dataFromClient.type === typesDef.WHITEBOARD_DRAW) {
+      console.log('Whiteboard event.')
+
+      // Store events as they arrive to roomspecific array
+      roomToEvents[roomId].push(dataFromClient)
+
+      broadcastMessageToRoom(dataFromClient, roomId)
+    }
+    else {
+      console.log('Unknown event when user belonged to a room')
+      console.log(userId)
+      console.log(dataFromClient)
+    }
   }
 }
 
